c6e17147f4b18483936058fa43d96349aab8ecf8
unknown <fernandocfbf@al.insper.edu.br>
2019-04-18 12:41:11 -0300

    Adicionei funções de combate

--- ep1.py
+++ ep1.py
@@ -24,8 +24,9 @@ def carregar_cenarios():
          },
         "Ir para a sala de estar":{
             'titulo':  'Pico do sossego',
-            'descricao' : 'Você está assistindo TV e encontrou o Sleep Monster \n'
-            'se você perder o combate dormirá profundamente!',
+            'descricao' : 'Você está assistindo TV e encontrou o Sleep Monster, \n'
+            'se você perder o combate seu sono será horrivél e você acordará'
+            ' com menos HP pela manhã!',
             'opcoes': {
                 'lutar': ' combate',
                 'fugir': ' sair correndo'
@@ -34,9 +35,7 @@ def carregar_cenarios():
     
         "lutar": {
             "titulo": "A jornada",
-            "descricao": "Você foi gravemente ferido e caiu em um sono profundo"
-            ". Agora acordou "
-            "e está chegando no insper sem ter feito o EP",
+            "descricao": "Você acordou e está chegando no Insper",
             "opcoes": {
                 "biblioteca": " pegar um livro",
                 "quarto andar": " pegar uma mala",
@@ -45,13 +44,11 @@ def carregar_cenarios():
         },
         "fugir": {
             "titulo": "Tiro de 100 metros",
-            "descricao": "Você correu mas o ele te pegou e agora você caiu \n"
-            'em um sono profundo. Agora acordou \n'
-            "e está chegando no insper sem ter feito o EP",
+            "descricao": "Você correu e se escondeu no seu quarto"
+            ', em cima da mesa você encotrou um energético! Agora isso está'
+            ' no seu inventário.',
             "opcoes": {
-                "biblioteca": " pegar um livro",
-                "quarto andar": " pegar uma mala",
-                "andar professor": " ir para o andar do professor"
+                "lutar": " enfrentar o SleepMonster"
                 
             }
         },
@@ -102,12 +99,10 @@ def main():
     print("Na hora do sufoco!")
     print("------------------")
     print()
-    print("Parecia uma boa idéia: vou só jogar um pouquinho/assistir Netflix/"
-        "embaçar em geral. Amanhã eu começo o EP. Mas isso não deu certo...")
+    print("Parecia uma boa idéia: vou enrolar, assistir Netflix,"
+        " embaçar em geral. Amanhã eu falo com o professor sobre o EP.")
     print()
-    print("É o dia de entregar o EP e você está muuuuito atrasado! Você está "
-        "na entrada do Insper, e quer procurar o professor para pedir um "
-        "adiamento do EP (boa sorte...)")
+    print("Sua missão será enfrentar o professor e conseguir adiar o EP (boa sorte)")
     print()
 
     cenarios, nome_cenario_atual = carregar_cenarios()
@@ -120,16 +115,16 @@ def main():
         print(cenario_atual['descricao'])
         
         vidaP1 = 100
-        lista_ataque_P1 = ['Golpe dos chutes mortais'] 
-        lista_de_alimentos = []
+        lista_ataque_P1 = ['Chutes mortais'] 
+        dicionario_de_alimentos = {}
         
         def define_valor_ataque(escolha):
-            if escolha == 'Golpe dos chutes mortais':
-                ataqueP1 = random.randint(0,10)
-            elif escolha == 'Golpe das mordidas profundas':
-                ataqueP1 = random.randint(5,35)
-            elif escolha == 'Golpe dos socos pesados':
-                ataqueP1 = random.randint(20,75)
+            if escolha == 'Chutes mortais':
+                ataqueP1 = random.randint(20,35)
+            elif escolha == 'Mordidas sangrentas':
+                ataqueP1 = random.randint(25,60)
+            elif escolha == 'Socos em chamas':
+                ataqueP1 = random.randint(50,100)
             
             return ataqueP1
         
@@ -139,6 +134,8 @@ def main():
                 soma = 60
             elif escolha == 'gaytorade':
                 soma = 45
+            elif escolha == 'energético':
+                soma = 20
             return soma
             
     
@@ -160,50 +157,120 @@ def main():
             else:
                 print("Sua indecisão foi sua ruína!")
                 game_over = True
+    
+# combate do SleepMonster
                 
         if nome_cenario_atual == 'lutar':
             ataque_SleepMonster = 51
             vida_SleepMonster = 100
             print()
-            print('---COMBATE---')
+            print('COMBATE')
             print()
             while vidaP1 > 50 and vida_SleepMonster > 0:
-                print('Seu turno')
-                if len(lista_de_alimentos) > 0: 
+                while len(dicionario_de_alimentos) > 0: 
+                    print('SEU TURNO')
                     print()
                     comer = input('Antes de atacar, digite "sim" se '
                                   'você deseja comer alguma coisa ou "não" para continuar: ')
                     if comer == 'sim':
                         print('Você pode comer:')
-                        for item in lista_de_alimentos:
-                            print(item)
-                            print()
-                        comendo = input('Digite o que deseja comer: ')
+                        for e,i in dicionario_de_alimentos.items():
+                            print('{0}(x{1})'.format(e,i))
+                            comendo = input('Digite o que deseja comer: ')
+                        del dicionario_de_alimentos[comendo]
+                        if comendo == 'energético':
+                            ataque_SleepMonster = random.randint(0,10)
+                                
                         somaVida = define_valor_alimentos(comendo)
                         vidaP1 += somaVida
-                else:
-                    print()
-                    
+                    elif comer == 'não':
+                        print()
+                        break
+                break
+                
+            while vidaP1 > 50 and vida_SleepMonster > 0: 
+                print('---SEU TURNO---')
                 print('Seu HP:', vidaP1)
                 print('Adversário HP:', vida_SleepMonster)
-                print()
                 print('Seu ataques disponíveis são:')
                 for e in lista_ataque_P1:
                     print(e)
+                    ataques = input('Digite o ataque que deseja usar: ')
+                    ataqueP1 = define_valor_ataque(ataques)
+                    print('Você o atingiu com {0} pontos '
+                    'de dano'.format(ataqueP1))
+                    print('---TURNO DO ADVERSÁRIO---')
+                    vida_SleepMonster -= ataqueP1
+                    print('Seu HP:', vidaP1)
+                    print('Adversário HP:', vida_SleepMonster)
+                    print('Você foi atingido com {0} pontos '
+                    'de dano '.format(ataque_SleepMonster))
+                    vidaP1 -= ataque_SleepMonster
                     print()
-                ataques = input('Digite o ataque que deseja usar: ')
-                ataqueP1 = define_valor_ataque(ataques)
-                print('Você o atingiu com {0} pontos de dano'.format(ataqueP1))
-                print()
-                print('Turno do adversário')
-                print(vidaP1, vida_SleepMonster)
-                print()
-                print('Você foi atingido com {0} pontos de dano '
-                'e não resistiu, você foi gravemente ferido e '
-                'caiu em um sono profundo'.format(ataque_SleepMonster))
-                vida_SleepMonster -= ataqueP1
-                vidaP1 -= ataque_SleepMonster
-                print()
+            
+            if vidaP1 <= 50:
+                print('Você foi gravemente ferido e caiu em um sono cheio de pesadelos!'
+                ' Sua manhã será terrível')
+            else:
+                print('Você venceu a batalha e teve um ótimo sono! Pela manhã'
+                ' você acordará perfeitamente descansado!')
+                vidaP1 += 15
+                
+# combate final
+                
+        elif nome_cenario_atual == 'lutar contra o professor':
+            ataque_Professor = random.randint(20,30)
+            vida_Professor = 150
+            print()
+            print('COMBATE')
+            print()
+            while vidaP1 > 50 and vida_Professor > 0:
+                while len(dicionario_de_alimentos) > 0: 
+                    print('SEU TURNO')
+                    print()
+                    comer = input('Antes de atacar, digite "sim" se '
+                                  'você deseja comer alguma coisa ou "não" para continuar: ')
+                    if comer == 'sim':
+                        print('Você pode comer:')
+                        for e,i in dicionario_de_alimentos.items():
+                            print('{0}(x{1})'.format(e,i))
+                            comendo = input('Digite o que deseja comer: ')
+                        del dicionario_de_alimentos[comendo]
+                                
+                        somaVida = define_valor_alimentos(comendo)
+                        vidaP1 += somaVida
+                    elif comer == 'não':
+                        print()
+                        break
+                break
+                
+            while vidaP1 > 0 and vida_SleepMonster > 0: 
+                print('---SEU TURNO---')
+                print('Seu HP:', vidaP1)
+                print('Adversário HP:', vida_Professor)
+                print('Seu ataques disponíveis são:')
+                for e in lista_ataque_P1:
+                    print(e)
+                    ataques = input('Digite o ataque que deseja usar: ')
+                    ataqueP1 = define_valor_ataque(ataques)
+                    print('Você o atingiu com {0} pontos '
+                    'de dano'.format(ataqueP1))
+                    print('---TURNO DO ADVERSÁRIO---')
+                    vida_Professor -= ataqueP1
+                    print('Seu HP:', vidaP1)
+                    print('Adversário HP:', vida_Professor)
+                    print('Você foi atingido com {0} pontos '
+                    'de dano '.format(ataque_Professor))
+                    vidaP1 -= ataque_Professor
+                    print()
+            
+            if vidaP1 <= 0:
+                print('GAME OVER')
+            elif vida_Professor <= 0:
+                print('Você conseguiu!!!!!!!! \n'
+                'o EP será adiado para sempre')
+            break
+                                    
         
 
     print("Você morreu!")
